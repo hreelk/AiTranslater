87db55d73abc09663ebe4c3c481ee0d53418b1fd
Hreelk <louis.1207@yahoo.com.tw>
2023-07-20 11:56:30 +0800

    基本完成

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import tkinter as tk
 from docx import Document
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import pyautogui
 import time
+from docx import Document
+import pyperclip    
 
 def open_webpage():
 
@@ -52,26 +55,120 @@ def open_webpage():
     driver.quit()
     # 關閉視窗
     window.destroy()
-    #以debug模式打開chrome
+    # 打開chrome
     bat_file_path = 'start_chrome_debug.bat'
     subprocess.call(bat_file_path, shell=True)
     # 創建 ChromeOptions
     options = webdriver.ChromeOptions()
     options.add_experimental_option("debuggerAddress", "localhost:9222")
     # 創建瀏覽器驅動程式
+    print("創建瀏覽器驅動程式")
     driver = webdriver.Chrome(options=options)
     # 移動到網站
+    print("移動到網站")
     driver.get("https://chat.forefront.ai")
     print(driver.title)
     #等待網站開好
+    print("等")
     wait = WebDriverWait(driver, 10)
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, "text-th-primary-dark")))
-    time.sleep(2)
-    #再輸入框位置點左鍵
-    pyautogui.click(x=2128, y=1972)
-    #輸入
-    hi = "hi"
-    pyautogui.typewrite(hi)
+    time.sleep(3)
+    # 讀取Word檔案
+    print("讀取Word檔案")
+    doc = Document(file_path)
+    file_name = os.path.splitext(os.path.basename(file_path))[0]
+    docA = Document()
+    docB = Document()
+    print("讀取Word檔案")
+    # 儲存讀取到的文字
+    print("儲存讀取到的文字")
+    text = ""
+    selected_text = ""
+    # 讀取每個段落的內容
+    print("讀取每個段落的內容")
+    for paragraph in doc.paragraphs:
+        lines = paragraph.text.split('\n')
+        for line in lines:
+            if line:
+                print("for")
+                # 將段落文字添加到text中
+                text += line
+                # 將文字添加到文檔中
+                docA.add_paragraph(text + "\n")
+                #添加翻譯要求
+                print("翻譯成中文")
+                text="翻譯成繁體中文\n" + text
+                #將其複製到剪貼簿中
+                pyperclip.copy(text)
+                print(text)
+                #再輸入框位置點左鍵
+                pyautogui.click(x=2128, y=1972)
+                pyautogui.hotkey('ctrl', 'v')
+                time.sleep(1)
+                pyautogui.press('enter')
+                #等待答案
+                time.sleep (5)
+                #複製答案
+                print("複製")
+                # 移動到起始位置
+                pyautogui.moveTo(x=1658, y=726)
+                # 按住滑鼠左鍵
+                pyautogui.mouseDown(button='left')
+                # 移動到結束位置
+                pyautogui.moveTo(x=3326, y=806, duration=0.5)
+                # 釋放滑鼠左鍵
+                pyautogui.mouseUp(button='left')
+                # 複製
+                pyautogui.hotkey('ctrl', 'c')
+                # 貼上
+                # 從剪貼簿中獲取文字
+                selected_text = pyperclip.paste()
+                print(selected_text)
+                # 刪除特定文字
+                selected_text = selected_text.replace("GPT-3.5", "")
+                print(selected_text)
+                # 將文字添加到文檔中
+                docA.add_paragraph(selected_text + "\n")
+                docB.add_paragraph(selected_text + "\n")
+                # 清空text
+                text = ""
+                selected_text = ""
+                time.sleep(1)
+                # 移動到起始位置
+                pyautogui.moveTo(x=470, y=774)
+                pyautogui.click(button='left')
+                time.sleep(0.5)
+                pyautogui.moveTo(x=392, y=942)
+                pyautogui.click(button='left')
+                time.sleep(1)
+                # 重新整理網頁
+                pyautogui.press('f5')
+                #等待網站開好
+                print("等")
+                wait = WebDriverWait(driver, 10)
+                wait.until(EC.presence_of_element_located((By.CLASS_NAME, "text-th-primary-dark")))
+                time.sleep(3)
+
+    print("結束")
+    # 關閉網站
+    print("關閉")
+    driver.quit()           
+    #儲存翻譯
+    print("存檔")
+    file_nameB = file_name + "中文翻譯.docx"
+    file_name = file_name + "中日對照.docx"
+    file_path = os.path.join(folder_path, file_name)
+    file_pathB = os.path.join(folder_path, file_nameB)
+    print(file_path)
+    print(file_pathB)
+    # 儲存 Word 文件
+    docA.save(file_path)
+    docB.save(file_pathB)
+    print("完成")
+    # 關閉瀏覽器
+    print("關閉")
+    pyautogui.moveTo(x=3773, y=38)
+    pyautogui.click(button='left')
 
 
 
